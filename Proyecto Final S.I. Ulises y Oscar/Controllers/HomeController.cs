88b9b4cea1798a35c8ad168b9ae8797f29e5eecd
ulimx <ulimx@ULISES-LAPTOP>
2022-11-28 13:06:08 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto Final S.I. Ulises y Oscar/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Proyecto_Final_S.I._Ulises_y_Oscar.Models;
+
+namespace Proyecto_Final_S.I._Ulises_y_Oscar.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult APrivacidad()
+		{
+			return View();
+		}
+
+		public IActionResult ResponLegal()
+		{
+			return View();
+		}
+		public IActionResult NormasyEstandares()
+		{
+			return View();
+		}
+
+		public IActionResult Seguridad()
+		{
+			return View();
+		}
+
+		public IActionResult Cifrado()
+		{
+			return View();
+		}
+
+		public IActionResult Integridad()
+		{
+			return View();
+		}
+
+		public IActionResult Criptografia()
+		{
+			return View();
+		}
+
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ Proyecto Final S.I. Ulises y Oscar/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proyecto_Final_S.I._Ulises_y_Oscar.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
